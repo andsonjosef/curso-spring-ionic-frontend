040f74848aeff7dfa203cf9efbcc22da653b4b24
andsonjosef <andson.josef@gmail.com>
2018-08-25 14:26:27 -0300

    disabling menuin the login screen

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, IonicPage } from 'ionic-angular';
+import { NavController, IonicPage, MenuController } from 'ionic-angular';
 import { CategoriesPage } from '../categories/categories';
 
 @IonicPage()
@@ -9,9 +9,18 @@ import { CategoriesPage } from '../categories/categories';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public menu: MenuController) {
 
   }
+
+  ionViewWillEnter(){
+    this.menu.swipeEnable(false);
+  }
+
+  ionViewDidLeave(){
+    this.menu.swipeEnable(true);
+  }
+
 
   login(){
     this.navCtrl.setRoot("CategoriesPage");
