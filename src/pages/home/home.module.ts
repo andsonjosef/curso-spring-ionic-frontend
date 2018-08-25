f361ac7a5396eaaf1a8a7a4e001d30799fdf106d
andsonjosef <andson.josef@gmail.com>
2018-08-25 13:47:05 -0300

    lazy loading

---
+++ src/pages/home/home.module.ts
@@ -0,0 +1,10 @@
+import { NgModel } from "@angular/forms";
+import { NgModule } from "@angular/core";
+import { HomePage } from "./home";
+import { IonicPageModule } from "ionic-angular";
+
+@NgModule({
+    declarations: [HomePage],
+    imports: [IonicPageModule.forChild(HomePage)] 
+})
+export class HomeModule{}
