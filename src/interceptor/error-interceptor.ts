67949d17e88ffaa9cfa529ea42fc24a8c876c2d8
andsonjosef <andson.josef@gmail.com>
2018-09-03 11:44:06 -0300

    handling defaults error using alert

--- src/interceptor/error-interceptor.ts
+++ src/interceptor/error-interceptor.ts
@@ -2,12 +2,13 @@ import { Injectable } from "@angular/core";
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Observable } from "rxjs/Rx";
 import { StorageService } from "../services/storage.service";
+import { AlertController, Backdrop } from "ionic-angular";
 
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService){
+    constructor(public storage: StorageService, public alertCtrl: AlertController){
 
     }
 
@@ -26,14 +27,51 @@ export class ErrorInterceptor implements HttpInterceptor {
             console.log(errorObj);
 
             switch(errorObj.status){
+                case 401:
+                this.handle401();
+                break;
+
                 case 403:
                 this.handle403();
+                break;
+
+                default:
+                this.handleDefaultError(errorObj);
                 break;
             }
 
             return Observable.throw(errorObj);
         }) as any;
     } 
+
+    handleDefaultError(errorObj){
+        let alert = this.alertCtrl.create({
+            title: 'Error' + errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text:'Ok'
+                }
+            ]
+       });
+       alert.present();
+
+    }
+
+    handle401(){
+       let alert = this.alertCtrl.create({
+            title: 'Error 401: Authentication failed',
+            message: 'invalid email or password',
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text:'Ok'
+                }
+            ]
+       });
+       alert.present();
+    }
 
     handle403(){
         this.storage.setLocalUser(null);
